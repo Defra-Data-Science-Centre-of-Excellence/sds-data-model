e96905801d9e111addd07512463efe28c529986c
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2022-04-06 09:06:27 +0100

    initial commit

---
+++ src/sds_data_model/constants.py
@@ -0,0 +1,12 @@
+BNG = "OSGB36 / British National Grid"
+GRID_SIZE = 10
+HEIGHT = 10_000
+WIDTH = 10_000
+OUT_SHAPE = (HEIGHT, WIDTH)
+
+
+GMD = "{http://www.isotc211.org/2005/gmd}"
+GML = "{http://www.opengis.net/gml/3.2}"
+GMX = "{http://www.isotc211.org/2005/gmx}"
+GCO = "{http://www.isotc211.org/2005/gco}"
+CHARACTER_STRING = GCO + "CharacterString"
