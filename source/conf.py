b541eb063c9d1451279bb03e0bcea8c5c44fae3b
Lara Spearman <lara.spearman@defra.gov.uk>
2022-07-18 10:20:28 +0100

    add datetime and remove myst_parserer

--- source/conf.py
+++ source/conf.py
@@ -10,6 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+from datetime import datetime
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
@@ -34,7 +35,6 @@ extensions = [
 	 "sphinx.ext.napoleon",
      "sphinx.ext.intersphinx",
      "sphinx.ext.githubpages",
-     "myst_parser",
 
 ]
 
