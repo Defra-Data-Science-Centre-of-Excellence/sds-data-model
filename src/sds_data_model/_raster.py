55bc2ede1982d61c7417f2c21fd1dcf3fc5c3796
JamesKeny0n <jimikenyon@icloud.com>
2022-09-06 11:52:00 +0100

    fix again

--- src/sds_data_model/_raster.py
+++ src/sds_data_model/_raster.py
@@ -6,11 +6,12 @@ from xarray import DataArray, Dataset
 
 from sds_data_model.constants import BNG_XMAX, BNG_XMIN, BNG_YMAX, CELL_SIZE
 
+
 def _check_cellsize(
     transform: Affine,
 ) -> Optional[bool]:
     if (
-        transform.a != CELL_SIZE or 
+        transform.a != CELL_SIZE or
         -transform.e != CELL_SIZE
     ):
         return True
@@ -22,14 +23,15 @@ def _check_shape_and_extent(
     raster: Dataset,
 ) -> Optional[bool]:
     if (
-        raster.rio.shape != (BNG_YMAX/CELL_SIZE, BNG_XMAX/CELL_SIZE) or 
-        raster.rio.transform().f != BNG_YMAX or 
+        raster.rio.shape != (BNG_YMAX/CELL_SIZE, BNG_XMAX/CELL_SIZE) or
+        raster.rio.transform().f != BNG_YMAX or
         raster.rio.transform().c != BNG_XMIN
     ):
         return True
     else:
         return None
-    
+
+
 def _data_array_to_dataset(
     raster: DataArray,
     data: ndarray,
@@ -38,15 +40,15 @@ def _data_array_to_dataset(
     return DataArray(
         data=data,
         coords={
-            raster.rio.grid_mapping : 
+            raster.rio.grid_mapping:
             raster[raster.rio.grid_mapping].rio.update_attrs(
-                {"GeoTransform" : geotransform}
+                {"GeoTransform": geotransform}
             )
         },
         dims=tuple(raster.dims),
     ).to_dataset(name=raster.coords['variable'].data.tolist())
-    
-        
+
+
 def _get_resample_shape(
     raster: DataArray,
 ) -> ndarray:
@@ -61,33 +63,38 @@ def _get_resample_shape(
 
 def _resample_cellsize(
     raster: DataArray,
-    categorical: bool=False,
+    categorical: bool = False,
 ) -> Dataset:
     if categorical:
-        interpolation=INTER_NEAREST_EXACT
+        interpolation = INTER_NEAREST_EXACT
     else:
-        interpolation=INTER_LINEAR_EXACT
+        interpolation = INTER_LINEAR_EXACT
     resampled = resize(
         src=raster.data,
         dsize=_get_resample_shape(raster)[::-1],
         interpolation=interpolation,
     )
-    
+
     return _data_array_to_dataset(
         raster=raster,
         data=resampled,
-        geotransform=f"{raster.rio.transform().c} {CELL_SIZE} 0 {raster.rio.transform().f} 0 -{CELL_SIZE}",
+        geotransform=(
+            f"{raster.rio.transform().c} {CELL_SIZE} 0 "
+            f"{raster.rio.transform().f} 0 -{CELL_SIZE}"
+        ),
     )
+
 
 def _check_no_data(
     raster: DataArray,
-    nodata: float=None,
+    nodata: float = None,
 ) -> None:
     if nodata is not None:
         raster.rio.write_nodata(nodata, inplace=True)
     else:
-        assert raster.rio.nodata is not None, "Input dataset does not have a nodata value. One must be provided."
-    
+        assert raster.rio.nodata is not None, \
+            "Input dataset does not have a nodata value. One must be provided."
+
 
 def _get_bng_offset(
     raster: DataArray,
@@ -101,10 +108,10 @@ def _get_bng_offset(
 
 def _to_bng_extent(
     raster: DataArray,
-    nodata: float=None
+    nodata: float = None
 ) -> Dataset:
     _check_no_data(raster, nodata)
-    
+
     bng = full(
         shape=(
             array(
@@ -112,7 +119,7 @@ def _to_bng_extent(
             ) / CELL_SIZE
         ).astype("int"),
         fill_value=raster.rio.nodata,
-        dtype=raster.dtype   
+        dtype=raster.dtype
     )
 
     offset = _get_bng_offset(raster)
@@ -121,10 +128,9 @@ def _to_bng_extent(
         offset[0]:offset[0] + raster.shape[0],
         offset[1]:offset[1] + raster.shape[1]
     ] = raster.data
-    
+
     return _data_array_to_dataset(
         raster=raster,
         data=bng,
         geotransform=f"{BNG_XMIN} {CELL_SIZE} 0 {BNG_YMAX} 0 -{CELL_SIZE}",
     )
-
--- src/sds_data_model/raster.py
+++ src/sds_data_model/raster.py
@@ -55,8 +55,10 @@
 #     masks: Generator[MaskTile, None, None]
 #     metadata: Metadata
 
-#     def to_data_arrays(self) -> TiledDataArrayLayer:
-#         data_arrays = (mask.to_data_array(name=self.name) for mask in self.masks)
+#    def to_data_arrays(self) -> TiledDataArrayLayer:
+#        data_arrays = (
+#            mask.to_data_array(name=self.name) for mask in self.masks
+#        )
 
 #         return TiledDataArrayLayer(
 #             name=self.name,
@@ -66,25 +68,29 @@
 
 # temporary resample & reshape function
 from pathlib import Path
-from xarray import DataArray, Dataset, open_dataset
+from xarray import Dataset, open_dataset
 from rasterio.drivers import raster_driver_extensions
 
-from sds_data_model.constants import CELL_SIZE
-from sds_data_model._raster import _check_cellsize, _check_shape_and_extent, _resample_cellsize, _to_bng_extent
+from sds_data_model._raster import (
+    _check_cellsize,
+    _check_shape_and_extent,
+    _resample_cellsize,
+    _to_bng_extent,
+) 
 from sds_data_model._vector import _check_layer_projection
 
 
 def read_dataset_from_file(
     data_path: str,
-    categorical: bool=False, 
-    nodata: float=None,
-    band: int=1,
-    engine: str=None,
+    categorical: bool = False,
+    nodata: float = None,
+    band: int = 1,
+    engine: str = None,
     decode_coords="all",
     **data_kwargs,
 ) -> Dataset:
     suffix = Path(data_path).suffixes[0]
-    
+
     if engine:
         pass
     elif suffix == ".zarr":
@@ -92,7 +98,7 @@ def read_dataset_from_file(
     elif suffix[1:] in raster_driver_extensions().keys():
         engine = "rasterio"
         decode_coords = None
-    
+
     raster = open_dataset(
         data_path,
         engine=engine,
@@ -100,22 +106,22 @@ def read_dataset_from_file(
         mask_and_scale=False,
         **data_kwargs,
     )
-    
-    _check_layer_projection({"crs" : raster.rio.crs.to_string()})
-    
+
+    _check_layer_projection({"crs": raster.rio.crs.to_string()})
+
     if raster.band.ndim:
         raster = raster.sel(band=band)
-    
+
     if _check_cellsize(raster.rio.transform()):
         raster = _resample_cellsize(
             raster=raster.to_array().squeeze(),
             categorical=categorical,
         )
-    
+
     if _check_shape_and_extent(raster):
         raster = _to_bng_extent(
             raster=raster.to_array().squeeze(),
             nodata=nodata,
         )
-    
+
     return raster
