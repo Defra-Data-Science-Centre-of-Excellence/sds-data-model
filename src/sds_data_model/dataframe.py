2d2ca5a7c02dd0fdda866d8477c325f84f789675
Lara Spearman <lara.spearman@defra.gov.uk>
2022-10-05 13:15:31 +0100

    initial commit for call method to test working

--- src/sds_data_model/dataframe.py
+++ src/sds_data_model/dataframe.py
@@ -1,6 +1,9 @@
 from typing import Any, Dict, Generator, List, Optional, Tuple, TypeVar
 from pathlib import Path
+from inspect import ismethod, signature
+from typing_extensions import Self
 
+from pyspark.sql import DataFrame as SparkDataFrame
 from dataclasses import dataclass
 from pyspark.pandas import  read_excel,DataFrame, Series
 
@@ -14,6 +17,24 @@ from pyspark.sql import SparkSession
 from sds_data_model.metadata import Metadata
 from sds_data_model._vector import _get_metadata, _get_name # , _get_categories_and_dtypes,   _recode_categorical_strings
 
+from logging import getLogger, INFO, StreamHandler, Formatter
+
+# create logger and set level to info
+logger = getLogger("sds")
+logger.setLevel(INFO)
+
+# create stream handler and set level to info
+stream_handler = StreamHandler()
+stream_handler.setLevel(INFO)
+
+# create formatter
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to stream handler
+stream_handler.setFormatter(formatter)
+
+# add stream handler to logger
+logger.addHandler(stream_handler)
 
 spark = SparkSession.getActiveSession()
 
@@ -92,3 +113,35 @@ class DataFrameWrapper:
             data = data,
             meta= metadata
         )
+
+    @classmethod    
+    def call_method(
+        self: Self, 
+        method_name: str,
+        *args,
+        **kwargs,
+    ) -> Optional[Self]:
+      #assign method call to an object, so you can examine it before implementing anything
+        attribute = getattr(self.data, method_name)
+        #check if its a method
+        if ismethod(attribute):
+            #get the signature (what argument it takes, return type, etc. )
+            sig = signature(attribute)
+            #bind arguments to function arguments
+            arguments = sig.bind(*args, **kwargs).arguments
+            #create string to pass through graph generator
+            formatted_arguments = ",".join(f"{key}={value}" for key, value in arguments.items())
+            logger.info(f"Calling {attribute.__qualname__}({formatted_arguments})")
+            #return value, so now no longer dealing with object, but calling method
+            return_value = attribute(*args, **kwargs)
+        else:
+            #if not method, assume its a property (eg. crs)
+            logger.info(f"Calling {attribute.__qualname__}")
+            return_value = attribute
+        #check if method_name returns a dataframe, if so update self.data attribute with that new data
+        if isinstance(return_value, SparkDataFrame):
+            self.data = return_value
+            return self
+        # if doesn't return a dataframe, (eg. called display), don't want to overwrite data attribute with nothing, so don't edit
+        else:
+            return return_value
