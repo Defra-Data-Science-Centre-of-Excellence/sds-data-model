6f9936ede877be3f93c4a3ab4d3a114228a25172
Lara Spearman <lara.spearman@defra.gov.uk>
2022-10-05 13:21:15 +0100

    initial commit for call method to check working

--- src/sds_data_model/dataframe.py
+++ src/sds_data_model/dataframe.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict, Generator, List, Optional, Tuple, TypeVar
+from typing_extensions import Self
 from pathlib import Path
+from inspect import ismethod, signature
 
 from dataclasses import dataclass
 from pyspark.pandas import  read_excel,DataFrame, Series
+from pyspark.sql import DataFrame as SparkDataFrame
 
 from pyspark_vector_files import read_vector_files
 from pyspark_vector_files.gpkg import read_gpkg
@@ -16,6 +19,25 @@ from sds_data_model._vector import _get_metadata, _get_name # , _get_categories_
 
 
 spark = SparkSession.getActiveSession()
+
+from logging import getLogger, INFO, StreamHandler, Formatter
+
+# create logger and set level to info
+logger = getLogger("sds")
+logger.setLevel(INFO)
+
+# create stream handler and set level to info
+stream_handler = StreamHandler()
+stream_handler.setLevel(INFO)
+
+# create formatter
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to stream handler
+stream_handler.setFormatter(formatter)
+
+# add stream handler to logger
+logger.addHandler(stream_handler)
 
 # DataFrameWrapper is an upper bound for _DataFrameWrapper. Specifying bound means that _DataFrameWrapper will only be DataFrameWrapper or one of its subclasses. 
 _DataFrameWrapper = TypeVar("_DataFrameWrapper", bound = "DataFrameWrapper")
@@ -93,4 +115,34 @@ class DataFrameWrapper:
             meta= metadata
         )
 
-        
+    @classmethod
+    def call_method(
+        self: Self, 
+        method_name: str,
+        *args,
+        **kwargs,
+    ) -> Optional[Self]:
+      #assign method call to an object, so you can examine it before implementing anything
+        attribute = getattr(self.data, method_name)
+        #check if its a method
+        if ismethod(attribute):
+            #get the signature (what argument it takes, return type, etc. )
+            sig = signature(attribute)
+            #bind arguments to function arguments
+            arguments = sig.bind(*args, **kwargs).arguments
+            #create string to pass through graph generator
+            formatted_arguments = ",".join(f"{key}={value}" for key, value in arguments.items())
+            logger.info(f"Calling {attribute.__qualname__}({formatted_arguments})")
+            #return value, so now no longer dealing with object, but calling method
+            return_value = attribute(*args, **kwargs)
+        else:
+            #if not method, assume its a property (eg. crs)
+            logger.info(f"Calling {attribute.__qualname__}")
+            return_value = attribute
+        #check if method_name returns a dataframe, if so update self.data attribute with that new data
+        if isinstance(return_value, SparkDataFrame):
+            self.data = return_value
+            return self
+        # if doesn't return a dataframe, (eg. called display), don't want to overwrite data attribute with nothing, so don't edit
+        else:
+            return return_value    
