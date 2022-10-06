b1056e6caa9ac65632aabf227dbb784a05fc7f64
Lara Spearman <lara.spearman@defra.gov.uk>
2022-10-06 12:36:09 +0100

    add initial test for from_files using basic data

--- tests/test_DataFrameWrapper.py
+++ tests/test_DataFrameWrapper.py
@@ -1,31 +1,66 @@
-#sample pyspark test
+from pathlib import Path
+from typing import ContextManager, Optional, Tuple, Union
+
 import pytest
-from pytest import FixtureRequest, fixture
+from _pytest.fixtures import FixtureRequest
+from chispa.dataframe_comparer import assert_df_equality
+from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+from pytest import raises, FixtureRequest, fixture
 
-def do_word_counts(lines):
-    """ count of words in an rdd of lines """
+from sds_data_model.dataframe import DataFrameWrapper
 
-    counts = (lines.flatMap(lambda x: x.split())
-                  .map(lambda x: (x, 1))
-                  .reduceByKey(add)
-             ) 
-    results = {word: count for word, count in counts.collect()}
-    return results
+@fixture
+def spark_context() -> SparkSession:
+    """Local Spark context."""
+    return (
+        SparkSession.builder.master(
+            "local",
+        )
+        .appName(
+            "Test context",
+        )
+        .getOrCreate()
+    )
 
-pytestmark = pytest.mark.usefixtures("spark_context")
+def expected_name() -> str:
+    """Expected DataFrameWrapper name."""
+    return "Trial csv"
 
-def test_do_word_counts(spark_context):
-    """ test word couting
-    Args:
-        spark_context: test fixture SparkContext
-    """
-    test_input = [
-        ' hello spark ',
-        ' hello again spark spark'
-    ]
 
-    input_rdd = spark_context.parallelize(test_input, 1)
-    results = wordcount.do_word_counts(input_rdd)
+def expected_metadata() -> None:
+    """Expected DataFrameWrapper metadata."""
+    return None
+
+
+def expected_schema() :
+    """Expected DataFrameWrapper schema."""
+    return StructType([
+        StructField("_c0", StringType(), True),
+        StructField("_c1", StringType(), True)
+])
     
-    expected_results = {'hello':2, 'spark':3, 'again':1}  
-    assert results == expected_results
+def expected_data():
+    data = spark_context.read.csv("tests/data/test_data.csv")
+    return data
+
+
+def test_vector_layer_from_files(
+    #shared_datadir: Path,
+    expected_name: str,
+    expected_schema,
+    expected_metadata: None,
+    expected_data
+) -> None:
+    """Reading test data returns a DataFrameWrapper with expected values."""
+    data_path = "tests/data/test_data.csv"
+    received = DataFrameWrapper.from_files(
+        data_path=data_path,
+        name='Trial csv',
+    )
+    
+    assert received.name == expected_name
+    assert received.meta == expected_metadata
+    assert received.data.schema == expected_schema
+    assert_df_equality(received.data, expected_data)
