74fa5e208da5a67ebf4d34c278492984c073e8c9
Lara Spearman <lara.spearman@defra.gov.uk>
2022-10-06 11:09:47 +0100

    add trial test for spark

--- tests/test_DataFrameWrapper.py
+++ tests/test_DataFrameWrapper.py
@@ -1,31 +1,45 @@
-#sample pyspark test
+from pathlib import Path
+from typing import ContextManager, Optional, Tuple, Union
+
 import pytest
-from pytest import FixtureRequest, fixture
+from _pytest.fixtures import FixtureRequest
+from chispa.dataframe_comparer import assert_df_equality
+from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.sql import SparkSession
+from pytest import raises
+import pandas as pd
 
-def do_word_counts(lines):
-    """ count of words in an rdd of lines """
+# def test__create_paths_sdf(
+#     spark_context: SparkSession,
+#     name: str,
+#     expected_name: SparkDataFrame,
+# ) -> None:
+#     """Returns the expected SparkDataFrame of paths."""
+#     paths_sdf = _create_paths_sdf(
+#         spark=spark_context,
+#         paths=(first_fileGDB_path,),
+#     )
+#     assert_df_equality(paths_sdf, expected_paths_sdf)
 
-    counts = (lines.flatMap(lambda x: x.split())
-                  .map(lambda x: (x, 1))
-                  .reduceByKey(add)
-             ) 
-    results = {word: count for word, count in counts.collect()}
-    return results
+def test_call_method(
+    spark_context: SparkSession,
+    #method_name: str, 
+) -> None:
+    """_summary_
 
-pytestmark = pytest.mark.usefixtures("spark_context")
-
-def test_do_word_counts(spark_context):
-    """ test word couting
     Args:
-        spark_context: test fixture SparkContext
+        spark_context (SparkSession): _description_
+        method_name (str): _description_
     """
-    test_input = [
-        ' hello spark ',
-        ' hello again spark spark'
-    ]
+    expected_data = {'a':[1,2],
+       'b':[3,4]}
+    expected_df= pd.DataFrame(expected_data)
+    sparkDF =spark_context.createDataFrame(expected_df)
+    edit_data = {'a':[1,2,3],
+        'b':[3,4,5]}
 
-    input_rdd = spark_context.parallelize(test_input, 1)
-    results = wordcount.do_word_counts(input_rdd)
-    
-    expected_results = {'hello':2, 'spark':3, 'again':1}  
-    assert results == expected_results
+    edit_df= pd.DataFrame(edit_data)
+    sparkDF_edit =spark_context.createDataFrame(edit_df)
+    sparkDF_edited = sparkDF_edit.limit(2)
+
+    assert_df_equality(sparkDF, sparkDF_edited)
