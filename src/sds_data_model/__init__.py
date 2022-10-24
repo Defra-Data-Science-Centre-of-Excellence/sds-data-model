3ffaef87fdd699ff968fc799ebe5ba2311913dca
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2022-10-24 12:14:39 +0100

    fix line lengths, trailing whitespaces, and typos

--- src/sds_data_model/__init__.py
+++ src/sds_data_model/__init__.py
@@ -8,7 +8,8 @@ Part 1: A breakdown of the process
 Reading in vector data
 ----------------------
 
-Read in a vector file as a Spark DataFrame using `pyspark_vector_files.read_vector_files()`:
+Read in a vector file as a Spark DataFrame using
+`pyspark_vector_files.read_vector_files()`:
 
 .. code-block:: python
 
@@ -19,9 +20,10 @@ Read in a vector file as a Spark DataFrame using `pyspark_vector_files.read_vect
 
 Filename pattern matching
 ^^^^^^^^^^^^^^^^^^^^^^^^^
-    
-Optionally, `kwargs` can be used to affect the behaviour of `pyspark_vector_files.read_vector_files()`. 
-For example, patterns and extensions can be used to help identify the exact paths of input files:
+
+Optionally, `kwargs` can be used to affect the behaviour of
+`pyspark_vector_files.read_vector_files()`. For example, patterns and extensions can
+be used to help identify the exact paths of input files:
 
 .. code-block:: python
 
@@ -37,14 +39,14 @@ For example, patterns and extensions can be used to help identify the exact path
 Calling pyspark methods
 -----------------------
 
-The `DataFrameWrapper` class has a generic method named `call_method` which will allow the user to 
-call any(?) valid pyspark DataFrame method on the dataframe.
+The `DataFrameWrapper` class has a generic method named `call_method` which will allow
+the user to call any(?) valid pyspark DataFrame method on the dataframe.
 
 `filter`
 ^^^^^^^^
 
-Rows can be filtered from a spark data frame using SQL-like expressions. For example to filter attributes
-based on name matches in a single column:
+Rows can be filtered from a spark data frame using SQL-like expressions. For example to
+filter attributes based on name matches in a single column:
 
 .. code-block:: python
 
@@ -84,12 +86,14 @@ Columns can be selected as follows:
         data_path = '/path/to/files/'
     ).call_method('select', [col('cola'), col('colb')])
 
-Note how the strings of the column names require wrapping in `pyspark.sql.functions.col`.
+Note how the strings of the column names require wrapping in
+`pyspark.sql.functions.col`.
 
 `join`
 ^^^^^^
 
-Both aspatial and spatial data can be joined to to the vector data once it is in the `DataFrameWrapper` class. 
+Both aspatial and spatial data can be joined to to the vector data once it is in the
+`DataFrameWrapper` class.
 
 Aspatial:
 
@@ -97,15 +101,15 @@ Aspatial:
 
     extra_data = spark.read.option("header",True) \
      .csv('/path/to/my_data.csv')
-    
+
     sdf = DataFrameWrapper.from_files(
         name = 'vector_name',
         data_path = '/path/to/files/'
     ).call_method('join', extra_data, 'name', 'left')
-    
-Here, 'name' and 'left' are the the extra arguments passed to `join`s `on` and `how` parameters. Given everything #
-passed to `call_method` is positional, these need to be in the correct order, with optional arguments skipped with 
-`None` as appropriate. 
+
+Here, 'name' and 'left' are the the extra arguments passed to `join`s `on` and `how`
+parameters. Given everything passed to `call_method` is positional, these need to be in
+the correct order, with optional arguments skipped with `None` as appropriate.
 
 Spatial:
 
@@ -114,19 +118,20 @@ Spatial:
     sdf1 = DataFrameWrapper.from_files(
         name = 'vector_name',
         data_path = '/path/to/files/'
-    
+
     sdf2 = DataFrameWrapper.from_files(
         name = 'vector_name',
         data_path = '/path/to/files/'
     ).call_method('join', sdf1.data, 'name', 'left')
-    
-Note how the `data` object from the DataFrameWrapper classed `sdf1` is selected within the `join` method.
+
+Note how the `data` object from the DataFrameWrapper classed `sdf1` is selected within
+the `join` method.
 
 Chaining Methods
 ^^^^^^^^^^^^^^^^
 
-The `call_method` method has been designed so that methods can be chained together. 
-For example, `select` then `filter` can be combined. The key thing to note here 
+The `call_method` method has been designed so that methods can be chained together.
+For example, `select` then `filter` can be combined. The key thing to note here
 is that the Spark DataFrame within the DataFrameWrapper is updated in-place. For
 example:
 
@@ -137,20 +142,20 @@ example:
         data_path = '/path/to/files/') \
         .call_method('select', [col('cola'), col('colb')]) \
         .call_method('filter', 'cola == "vala" or cola == "valb"')
-            
+
 
 Indexing
 --------
 
 Once vector files have been read in and transformations complete, the next step
 is to index the data. This process adds two additional columns to the dataframe
-stored within `data` within the DataFrameWrapper. `bng` contains the two 
-British National Grid grid letters relevant to that featue and `bounds` is a 
+stored within `data` within the DataFrameWrapper. `bng` contains the two
+British National Grid grid letters relevant to that feature and `bounds` is a
 tuple with British National Grid coordinates for the cell relating to the grid
 letters. The indexing functions are called from the `bng_indexer` module.
 
 The `index` function has several arguments, but only `resolution` requires user
-input as the rest have defaults. Recommended resolution is `100_000`. 
+input as the rest have defaults. Recommended resolution is `100_000`.
 
 For example:
 
@@ -158,10 +163,10 @@ For example:
 
     sdf.index(resolution = 100_000)
 
-The above code would update the `sdf` object, providing the wrapped data frame
+The above code would update the `sdf` object, providing the wrapped dataframe
 with the additional columns.
 
-Some of the other arguments allow the user to provide custom column names for the 
+Some of the other arguments allow the user to provide custom column names for the
 geometry column as an input, and the two output columns:
 
 .. code-block:: python
@@ -170,17 +175,17 @@ geometry column as an input, and the two output columns:
          geometry_column_name = "custom_geom",
          index_column_name = "custom_index",
          bounds_column_name = "custom_bounds")
-         
+
 **TODO: how do variations in `how` work?**
 
 Once the spatial index has been created, the DataFrameWrapper is ready for writing
-to a zarr file. 
+to a zarr file.
 
 Writing to zarr
 ---------------
 
 The final stage of the data model pipeline is to rasterise data to a standard
-10 m grid in British National Grid projection, and write the data to a zarr 
+10 m grid in British National Grid projection, and write the data to a zarr
 file. This can be done with the `to.zarr()` method:
 
 .. code-block:: python
@@ -201,7 +206,7 @@ been changed from the defaults:
 
 
 Part 2: A full workflow
-=============== 
+=======================
 
 The whole workflow can be pulled together like this:
 
