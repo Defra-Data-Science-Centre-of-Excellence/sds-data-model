9862f2d4c03c0e6b2672da9115acf3df7a9bd60d
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2022-10-24 15:14:41 +0100

    Update module docstring

--- src/sds_data_model/__init__.py
+++ src/sds_data_model/__init__.py
@@ -2,37 +2,109 @@
 
 The following usage examples assume a pyspark enabled environment.
 
-Part 1: A breakdown of the process
-==================================
+Part 1: The `DataFrameWrapper` class
+========================
 
-Reading in vector data
-----------------------
+The SDS data model DataFrameWrapper class is a thin wrapper around a
+`Spark DataFrame`_.
 
-Read in a vector file as a Spark DataFrame using
-`pyspark_vector_files.read_vector_files()`:
+It consists of:
+
+- A name
+- A Spark DataFrame instance
+- An instance of the SDS data model `Metadata`_ class
+
+Wrapping a Spark DataFrame in this way allows us to bind data and metadata together,
+abstract common patterns to methods, and capture transformations.
+
+Reading in data
+---------------
+
+You can construct a DataFrameWrapper instance from several different spatial and
+aspatial file formats using the `from_files`_ alternative constructor. `from_files`
+will use a different reader depending on the file extension.
+
+- For Excel Workbook (`.xlsx`, `.xls`, `.xlsm`, and `.xlsb`) or OpenDocument Format
+(`.odf`, `.ods`, `.odt`) files, it will use `pyspark.pandas.read_excel`_.
+- For CSV, it will use `pyspark.sql.DataFrameReader.csv`_.
+- For JSON, it will use `pyspark.sql.DataFrameReader.json`_.
+- For Parquet, it will use `pyspark.sql.DataFrameReader.parquet`_.
+- For GeoPackage, it will use `pyspark_vector_files.gpkg.read_gpkg`_.
+
+It will assume that any other file type is a vector file and will try and use
+`pyspark_vector_files.read_vector_files`_ to read it.
+
+.. warning:: Databricks File System paths
+    The Excel, OpenDocument, CSV, JSON, and Parquet readers use Spark API Format
+    file paths, i.e. `dbfs:/path/to/file.ext`, whereas, the GeoPackage and other
+    vector file type readers use the File API Format, i.e. `/dbfs/path/to/file.ext`.
+
+Passing keyword arguments to the underlying reader
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+The `read_file_kwargs` argument allows you can pass `kwargs` to the underlying reader.
+
+For example, you can pass `kwargs` to `pyspark.pandas.read_excel`_ to read a specific
+section of an OpenDocument Spreadsheet:
 
 .. code-block:: python
 
-    sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/'
+    ods_df = DataFrameWrapper.from_files(
+        name="opendocument spreadsheet example",
+        data_path="dbfs:/path/to/file.ods",
+        read_file_kwargs={
+            "sheet_name": "1",
+            "skiprows": 10,
+            "header": [0, 1],
+            "nrows": 23,
+        },
     )
 
-Filename pattern matching
-^^^^^^^^^^^^^^^^^^^^^^^^^
-
-Optionally, `kwargs` can be used to affect the behaviour of
-`pyspark_vector_files.read_vector_files()`. For example, patterns and extensions can
-be used to help identify the exact paths of input files:
+Or you can pass `kwargs` to `pyspark.sql.DataFrameReader.csv`_ to read a CSV with a
+header:
 
 .. code-block:: python
 
-    sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/',
-        read_file_kwargs = {'pattern': 'filename_pattern*',
-        'suffix': '.ext'
-        }
+    csv_df = DataFrameWrapper.from_files(
+        name="csv example",
+        data_path="dbfs:/path/to/file.csv",
+        read_file_kwargs={
+            "header": True,
+            "inferSchema": True,
+        },
+    )
+
+Or you can pass `kwargs` to `pyspark_vector_files.gpkg.read_gpkg`_ to read a GeoPackage
+without returning the GeoPackage Binary Header:
+
+.. code-block:: python
+
+    gpkg_df = DataFrameWrapper.from_files(
+        name="geopackage example",
+        data_path="/dbfs/path/to/file.gpkg",
+        read_file_kwargs={
+            "drop_gpb_header": True,
+        },
+    )
+
+.. warning:: GeoPackage JDBC dialect
+    To read a GeoPackage using Spark's JDBC drivers, you will need to
+    register a custom mapping of GeoPackage to Spark Catalyst types.
+
+    See `register the geopackage dialect`_ for details.
+
+Or you can pass `kwargs` to `pyspark_vector_files.read_vector_files` to read three
+Shapefiles into single DataFrame:
+
+.. code-block:: python
+
+    vector_df = DataFrameWrapper.from_files(
+        name="vector name",
+        data_path="/dbfs/path/to/files",
+        read_file_kwargs={
+            "pattern": "filename_pattern*",
+            "suffix": ".ext",
+        },
     )
 
 
@@ -40,57 +112,74 @@ Calling pyspark methods
 -----------------------
 
 The `DataFrameWrapper` class has a generic method named `call_method` which will allow
-the user to call any(?) valid pyspark DataFrame method on the dataframe.
+the user to call `any valid Spark DataFrame method`_ on the underlying DataFrame
+instance.
+
+If the method returns a Spark DataFrame the underlying DataFrame instance
+will be updated, if not, the output of the method will be returned.
 
 `filter`
 ^^^^^^^^
 
-Rows can be filtered from a spark data frame using SQL-like expressions. For example to
-filter attributes based on name matches in a single column:
+The Spark DataFrame `filter`_ method allow you to select rows using SQL-like
+expressions. You can call this method by passing the name of the method, followed by
+the condition you want to filter on, to the `DataFrameWrapper`'s `call_method` method.
+
+For example, to filter attributes based on name matches in a single column:
 
 .. code-block:: python
 
     sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/'
-    ).call_method('filter', 'col == "val"')
+        name="vector_name",
+        data_path="/path/to/files/"
+    ).call_method("filter", "col == 'val'")
 
 Multiple queries can be executed with `or`:
 
 .. code-block:: python
 
     sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/'
-    ).call_method('filter', 'col == "vala" or col == "valb"')
+        name="vector_name",
+        data_path="/path/to/files/"
+    ).call_method("filter", "col == 'val_a' or col == 'val_b'")
 
 Or, to identify rows where values contain a certain string, you can use the following:
 
 .. code-block:: python
 
     sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/'
-    ).call_method('filter', 'col LIKE "%vala%"')
+        name="vector_name",
+        data_path="/path/to/files/"
+    ).call_method("filter", "col LIKE '%val_a%'")
 
 
 `select`
 ^^^^^^^^
 
-Columns can be selected as follows:
+The Spark DataFrame `select`_ method allow you to select columns by name. You can call
+this method by passing the name of the method, followed by the columns you want to
+select, to the `DataFrameWrapper`'s `call_method` method.
+
+For example, columns can be selected as follows:
 
 .. code-block:: python
 
     sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/'
-    ).call_method('select', [col('cola'), col('colb')])
+        name="vector_name",
+        data_path="/path/to/files/"
+    ).call_method("select", [col("col_a"), col("col_b")])
 
-Note how the strings of the column names require wrapping in
-`pyspark.sql.functions.col`.
+.. warning::
+    Unlike the underlying method, column names cannot be passed as strings.
+
+    Instead, they must be wrapped in `pyspark.sql.functions.col`_.
 
 `join`
 ^^^^^^
+
+The Spark DataFrame `join`_ method allow you to join two Spark DataFrames. You can call
+this method by passing the name of the method, followed by the relevant arguments, to
+the `DataFrameWrapper`'s `call_method` method.
 
 Both aspatial and spatial data can be joined to to the vector data once it is in the
 `DataFrameWrapper` class.
@@ -99,17 +188,22 @@ Aspatial:
 
 .. code-block:: python
 
-    extra_data = spark.read.option("header",True) \
-     .csv('/path/to/my_data.csv')
+    extra_data = spark.read.option("header", True) \
+     .csv("/path/to/my_data.csv")
 
     sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/'
-    ).call_method('join', extra_data, 'name', 'left')
+        name="vector_name",
+        data_path="/path/to/files/"
+    ).call_method("join", extra_data, "name", "left")
 
-Here, 'name' and 'left' are the the extra arguments passed to `join`s `on` and `how`
-parameters. Given everything passed to `call_method` is positional, these need to be in
-the correct order, with optional arguments skipped with `None` as appropriate.
+Here, `name` and `left` are the the extra arguments passed to `join`s `on` and `how`
+parameters. The above is equivalent to:
+
+    sdf = DataFrameWrapper.from_files(
+        name="vector_name",
+        data_path="/path/to/files/"
+    ).call_method("join", other=extra_data, on="name", how="left")
+
 
 Spatial:
 
@@ -138,21 +232,21 @@ example:
 .. code-block:: python
 
     sdf = DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/') \
-        .call_method('select', [col('cola'), col('colb')]) \
-        .call_method('filter', 'cola == "vala" or cola == "valb"')
+        name="vector_name",
+        data_path="/path/to/files/") \
+        .call_method("select", [col("col_a"), col("col_b")]) \
+        .call_method("filter", "col_a == 'val_a' or col_a == 'val_b'")
 
 
 Indexing
 --------
 
 Once vector files have been read in and transformations complete, the next step
-is to index the data. This process adds two additional columns to the dataframe
+is to index the data. This process adds two additional columns to the DataFrame
 stored within `data` within the DataFrameWrapper. `bng` contains the two
 British National Grid grid letters relevant to that feature and `bounds` is a
 tuple with British National Grid coordinates for the cell relating to the grid
-letters. The indexing functions are called from the `bng_indexer` module.
+letters. The indexing functions are called from the `bng_indexer`_ library.
 
 The `index` function has several arguments, but only `resolution` requires user
 input as the rest have defaults. Recommended resolution is `100_000`.
@@ -161,9 +255,9 @@ For example:
 
 .. code-block:: python
 
-    sdf.index(resolution = 100_000)
+    sdf.index(resolution=100_000)
 
-The above code would update the `sdf` object, providing the wrapped dataframe
+The above code would update the `sdf` object, providing the wrapped DataFrame
 with the additional columns.
 
 Some of the other arguments allow the user to provide custom column names for the
@@ -171,10 +265,10 @@ geometry column as an input, and the two output columns:
 
 .. code-block:: python
 
-    sdf.index(resolution = 100_000,
-         geometry_column_name = "custom_geom",
-         index_column_name = "custom_index",
-         bounds_column_name = "custom_bounds")
+    sdf.index(resolution=100_000,
+         geometry_column_name="custom_geom",
+         index_column_name="custom_index",
+         bounds_column_name="custom_bounds")
 
 **TODO: how do variations in `how` work?**
 
@@ -186,12 +280,12 @@ Writing to zarr
 
 The final stage of the data model pipeline is to rasterise data to a standard
 10 m grid in British National Grid projection, and write the data to a zarr
-file. This can be done with the `to.zarr()` method:
+file. This can be done with the `to_zarr()` method:
 
 .. code-block:: python
 
-    sdf.to_zarr(path = '/path/to/out_directory/',
-           data_array_name = 'array_name')
+    sdf.to_zarr(path="/path/to/out_directory/",
+           data_array_name="array_name")
 
 
 As with the indexing function, custom column names can be provided if they have
@@ -199,10 +293,10 @@ been changed from the defaults:
 
 .. code-block:: python
 
-    sdf.to_zarr(path = '/path/to/out_directory/',
-           data_array_name = 'array_name',
-            index_column_name: str = "custom_index",
-            geometry_column_name: str = "custom_geometry")
+    sdf.to_zarr(path="/path/to/out_directory/",
+           data_array_name="array_name",
+            index_column_name="custom_index",
+            geometry_column_name="custom_geometry")
 
 
 Part 2: A full workflow
@@ -213,12 +307,60 @@ The whole workflow can be pulled together like this:
 .. code-block:: python
 
     DataFrameWrapper.from_files(
-        name = 'vector_name',
-        data_path = '/path/to/files/',
-        data_path = '/path/to/files/'
-    ).call_method('filter', 'cola == "vala" or cola == "valb"'
-    ).index(resolution = 100_000
-    ).to_zarr(path = '/path/to/out_directory/',
-           data_array_name = 'array_name')
+        name="vector_name",
+        data_path="/dbfs/path/to/files",
+        read_file_kwargs={
+            "pattern": "filename_pattern*",
+            "suffix": ".ext",
+        },
+    ).call_method("filter", "col_a == 'val_a' or col_a == 'val_b'"
+    ).index(resolution=100_000
+    ).to_zarr(path="/path/to/out_directory/",
+           data_array_name="array_name")
 
+.. _`Spark DataFrame`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrame.html
+
+.. _`Metadata`:
+
+.. _`from_files`:
+
+.. _`pyspark.pandas.read_excel`:
+    https://spark.apache.org/docs/latest/api/python/reference/pyspark.pandas/api/pyspark.pandas.read_excel.html
+
+.. _`pyspark.sql.DataFrameReader.csv`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrameReader.csv.html
+
+.. _`pyspark.sql.DataFrameReader.json`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrameReader.json.html
+
+.. _`pyspark.sql.DataFrameReader.parquet`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrameReader.parquet.html
+
+.. _`pyspark_vector_files.gpkg.read_gpkg`:
+    https://defra-data-science-centre-of-excellence.github.io/pyspark-vector-files/usage.html#read-a-layer-into-a-spark-dataframe
+
+.. _`pyspark_vector_files.read_vector_files`:
+    https://defra-data-science-centre-of-excellence.github.io/pyspark-vector-files/api.html
+
+.. _`register the geopackage dialect`:
+    https://defra-data-science-centre-of-excellence.github.io/pyspark-vector-files/usage.html#register-the-geopackage-dialect
+
+.. _`any valid Spark DataFrame method`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/pyspark.sql.html#dataframe-apis
+
+.. _`filter`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrame.filter.html
+
+.. _`select`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrame.select.html
+
+.. _`pyspark.sql.functions.col`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.functions.col.html
+    
+.. _`join`:
+    https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.sql.DataFrame.join.html
+
+.. _`bng_indexer`:
+    https://github.com/Defra-Data-Science-Centre-of-Excellence/bng-indexer
 """
