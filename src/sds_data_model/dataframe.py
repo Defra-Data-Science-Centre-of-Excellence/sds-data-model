4b2049112ec72b50257c2221704812f2675a96f0
James Duffy <james.duffy@defra.gov.uk>
2022-11-01 15:37:01 +0000

    Add checks for column names to the beginning of `to_zarr` Fixes #151

--- src/sds_data_model/dataframe.py
+++ src/sds_data_model/dataframe.py
@@ -249,6 +249,14 @@ class DataFrameWrapper:
         Returns:
             None
         """
+        colnames = self.data.columns
+        
+        if index_column_name not in colnames:
+            raise TypeError(f"{index_column_name} is not present in the data.")
+        
+        if geometry_column_name not in colnames:
+            raise TypeError(f"{geometry_column_name} is not present in the data.")
+        
         _create_dummy_dataset(
             path=path,
             data_array_name=data_array_name,
