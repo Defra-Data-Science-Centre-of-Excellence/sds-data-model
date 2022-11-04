a94ef4889cf0d2d65a50974be093f6e7bc20da3f
James Duffy <james.duffy@defra.gov.uk>
2022-11-04 11:30:42 +0000

    move conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 """Central place for test fixtures."""
-from tests.fixtures.fixtures_metadata import (
+from fixtures.fixtures_metadata import (
     expected_metadata,
     expected_without_keywords,
     expected_without_title,
