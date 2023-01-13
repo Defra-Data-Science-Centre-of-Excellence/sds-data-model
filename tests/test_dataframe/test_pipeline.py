36ed33aa953d9023605d52de044a01eb33ecb34d
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2023-01-13 13:22:12 +0000

    add new pipeline test

--- tests/test_dataframe/test_pipeline.py
+++ tests/test_dataframe/test_pipeline.py
@@ -1,55 +1,24 @@
 """Tests for the `DataFrameWrapper` pipeline."""
+from pathlib import Path
 
-# def test_pipeline(
-#     gpdf: GeoDataFrame,
-#     tmp_path: Path,
-#     expected_HL_array: NDArray[int8],
-#     expected_HM_array: NDArray[int8],
-# ) -> None:
-#     """Returns expected values for grid references HL and HM."""
-#     gpkg_path = tmp_path / "test.gpkg"
-#     zarr_path = tmp_path / "test.zarr"
+from chispa import assert_df_equality
+from pyspark.sql import DataFrame as SparkDataFrame
 
-#     gpdf.to_file(gpkg_path)
+from sds_data_model.dataframe import DataFrameWrapper
 
-#     pipeline = (
-#         VectorLayer.from_files(
-#             data_path=gpkg_path,
-#             name="test",
-#             convert_to_categorical=["category"],
-#         )
-#         .to_tiles()
-#         .to_dataset_as_raster(columns=["category"])
-#         .to_zarr(
-#             store=zarr_path,
-#             mode="w",
-#             compute=False,
-#         )
-#     )
-#     with ProgressBar():
-#         pipeline.compute()
 
-#     dataset = open_dataset(
-#         zarr_path,
-#         chunks={
-#             "northings": 10_000,
-#             "eastings": 10_000,
-#         },
-#         engine="zarr",
-#     )
+def test_pipeline(
+    tmp_path: Path,
+    spark_dataframe: SparkDataFrame,
+) -> None:
+    """End to end test."""
+    dfw = DataFrameWrapper.from_files(
+        data_path=str(tmp_path),
+        read_file_kwargs={
+            "suffix": ".shp",
+        },
+        name="test",
 
-#     received_HL_array = dataset["category"][range(0, 10_000), range(0, 10_000)].values
+    )
 
-#     assert array_equal(
-#         a1=received_HL_array,
-#         a2=expected_HL_array,
-#     )
-
-#     received_HM_array = dataset["category"][
-#         range(0, 10_000), range(10_000, 20_000)
-#     ].values
-
-#     assert array_equal(
-#         a1=received_HM_array,
-#         a2=expected_HM_array,
-#     )
+    assert_df_equality(dfw.data, spark_dataframe)
